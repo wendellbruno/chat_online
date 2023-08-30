78aa01539b67d4db7835a3884dc2f649540af6bf
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-08-30 14:49:18 -0300

    servidor iniciado
    servidor iniciado

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,17 @@
-let a: number = 2;
-let b: number = 2;
+import express from 'express';
+import {createServer} from 'http';
 
 
-console.log(a+b)
+//o servidor express vai rodar com base no servidor nativo do node
+const app = express();
+const http = createServer(app);
+
+app.get('/', (req,res) =>{
+    res.json('Teste')
+})
+
+
+http.listen(3000, () => console.log('App Rodando'));
+
+
+
---
+++ backend/src/model/socket.ts
@@ -0,0 +1,28 @@
+import { Server } from "../../node_modules/socket.io/dist/index";
+
+interface ServerToClientEvents {
+    noArg: () => void;
+    basicEmit: (a: number, b: string, c: Buffer) => void;
+    withAck: (d: string, callback: (e: number) => void) => void;
+  }
+  
+  interface ClientToServerEvents {
+    hello: () => void;
+  }
+  
+  interface InterServerEvents {
+    ping: () => void;
+  }
+  
+  interface SocketData {
+    name: string;
+    age: number;
+}
+
+
+export const io = new Server<
+    ClientToServerEvents, 
+    ServerToClientEvents, 
+    InterServerEvents, 
+    SocketData
+>();
